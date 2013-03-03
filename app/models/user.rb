26e77dad0e4e94b07d8d4c6c1a9ca0ce0cd1714c
Víctor Viruete Roldán <victor.viruete@diacode.com>
2013-03-03 15:12:55 -0800

    Modified the registration process to force user to select his native language

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,14 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name
-  # attr_accessible :title, :body
+  attr_accessible :email, :password, :password_confirmation, :remember_me, 
+    # Our custom fields
+    :first_name, :last_name, :native_language_id
 
   belongs_to :native_language, :class_name => "Language"
   has_and_belongs_to_many :interested_languages, 
     :class_name => "Language", 
     :join_table => "interests"
+
+  validates :native_language, :presence => true
 end
