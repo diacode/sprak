67a52f91d88c94886353ba0558e1ad853949d37f
Víctor Viruete Roldán <victor.viruete@diacode.com>
2013-03-02 22:07:15 -0800

    Added couple of languages to the seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Language.create(name: 'English')
+Language.create(name: 'Spanish')
