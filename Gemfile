26d06caf85ac2c28ea4db637c112f05ddd8b6987
Javier Cuevas <javi@diacode.com>
2013-03-02 17:45:53 -0800

    Fresh rails project

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.12'
+gem 'mysql2'
+gem 'jquery-rails'
+gem 'haml-rails'
+gem 'font-awesome-rails'
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
+end
+
+group :development do
+  gem 'thin'
+end
+
+# Deploy with Capistrano
+# gem 'capistrano'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sprak::Application.config.secret_token = '9ab1b93b9a75a8628c5222d43bc504c087a660c2c842c04a8c9250db2743aa1e8d44762457da247e642b060b0e7019883309048ee878ed6946bf91c9360fa066'
